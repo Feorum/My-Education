cabc6cd3180065ec37a613a3c1cfe2b82bbbd87a
Feorum <protoofeorum@gmail.com>
2026-05-24 23:35:08 +0300

    Another one button

--- src/Button.kt
+++ src/Button.kt
@@ -1,21 +1,24 @@
 import java.awt.Component
+import java.awt.Dialog
 import javax.swing.*
 
 fun main() {
     SwingUtilities.invokeLater {
-        val frame = JFrame("Окно с текстом и кнопками")
+        val frame = JFrame("Другое Окно с текстом и кнопками")
         frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
         frame.setSize(400, 300)
         frame.setLocationRelativeTo(null)
-
         val label = JLabel("Нажми кнопку")
         label.alignmentX = Component.CENTER_ALIGNMENT
 
         val button1 = JButton("Первая кнопка")
-        button1.alignmentX = Component.CENTER_ALIGNMENT
+        button1.alignmentX = Component.RIGHT_ALIGNMENT
 
         val button2 = JButton("Вторая кнопка")
-        button2.alignmentX = Component.CENTER_ALIGNMENT
+        button2.alignmentX = Component.RIGHT_ALIGNMENT
+
+        val button3 = JButton("Третья кнопка")
+        button3.alignmentX = Component.LEFT_ALIGNMENT
 
         button1.addActionListener {
             label.text = "Нажата первая кнопка"
@@ -23,6 +26,17 @@ fun main() {
 
         button2.addActionListener {
             label.text = "Нажата вторая кнопка"
+        }
+
+        button3.addActionListener {
+            label.text = "Нажата третья кнопка"
+            val dialog = JDialog(frame, "Поздравляем!", true)
+            dialog.setSize(350, 250)
+            dialog.setLocationRelativeTo(frame)
+
+            dialog.add(JLabel("Поздравляем, последняя кнопка нажата!", SwingConstants.CENTER))
+
+            dialog.isVisible = true
         }
 
         val panel = JPanel()
@@ -34,6 +48,8 @@ fun main() {
         panel.add(button1)
         panel.add(Box.createVerticalStrut(10))
         panel.add(button2)
+        panel.add(Box.createVerticalStrut(10))
+        panel.add(button3)
         panel.add(Box.createVerticalGlue())
 
         frame.contentPane = panel
